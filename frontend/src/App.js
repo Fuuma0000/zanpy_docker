8b319fa9f2e0e798b431c1c5bd7c3458022b2c18
Fuuma <masa81hiro69@gmail.com>
2023-05-14 02:53:45 +0900

    [add]最初にデータを持ってきてkeyを登録できるように

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,75 +5,49 @@ import MyAreaScreen from "./screens/MyAreaScreen";
 import MemoScreen from "./screens/MemoScreen";
 import SearchScreen from "./screens/SearchScreen";
 import { RecoilRoot } from "recoil";
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
   //keyを設定するための関数
-  function createData(area, type, name, states, memos, memoTime) {
-    return { area, type, name, states, memos, memoTime };
+  function createData(
+    ani_id,
+    area_id,
+    ani_type,
+    ani_name,
+    status_id,
+    memos,
+    memoTime
+  ) {
+    return { ani_id, area_id, ani_type, ani_name, status_id, memos, memoTime };
   }
 
   // メモ画面に行ってもデータが残っているようにするためにapp.jsにstateを設定
-  const [rows, setRows] = useState([
-    createData(
-      "ふれあい",
-      "うさぎ",
-      "キャロット",
-      null,
-      "どっか行った",
-      "10/10 10:00"
-    ),
-    createData(
-      "ふれあい",
-      "うさぎ",
-      "ぴょんすけ",
-      null,
-      "どりゃああああ",
-      "10/10 10:06"
-    ),
-    createData(
-      "ふれあい",
-      "うさぎ",
-      "ミッフィー",
-      null,
-      "ぽにょだよー",
-      "10/10 11:10"
-    ),
-    createData(
-      "ふれあい",
-      "うさぎ",
-      "ピーター",
-      null,
-      "ちくわの中身を覗いてしまった",
-      "10/10 12:00"
-    ),
-    createData(
-      "ふれあい",
-      "レッサーパンダ",
-      "ラスカル",
-      null,
-      "",
-      "10/11 10:00"
-    ),
-    createData(
-      "ふれあい",
-      "レッサーパンダ",
-      "メイ",
-      null,
-      "お腹すいた",
-      "10/11 14:00"
-    ),
-    createData("ふれあい", "リスざる", "ジュリアン", null, "", "10/11 18:00"),
-    createData("ふれあい", "リスざる", "ジュリアン", null, "", "10/11 20:00"),
-    createData("ふれあい", "リスざる", "ジュリアン", null, "", "10/11 23:00"),
-    createData(
-      "ふれあい",
-      "リスざるwww",
-      "ジュリアン",
-      null,
-      "",
-      "10/14 10:00"
-    ),
-  ]);
+  const [rows, setRows] = useState([]);
+
+  //初回レンダリング時にAPIからデータを取得する
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/myarea");
+        const dataWithKeys = response.data.message.map((item) =>
+          createData(
+            item.ani_id,
+            item.area_id,
+            item.ani_type,
+            item.ani_name,
+            item.status_id,
+            item.memos,
+            item.memoTime
+          )
+        );
+        setRows(dataWithKeys);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <BrowserRouter>
@@ -85,10 +59,7 @@ function App() {
           />
           <Route path={`/login`} element={<LoginScreen />} />
           <Route path={`/memo`} element={<MemoScreen />} />
-          <Route
-            path={`/search`}
-            element={<SearchScreen rows={rows} setRows={setRows} />}
-          />
+          <Route path={`/search`} element={<SearchScreen />} />
           <Route path={`/*`} element={<LoginScreen />} />
         </Routes>
       </RecoilRoot>
