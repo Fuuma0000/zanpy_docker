cdc0d64096ff1ce5a7ed546a05a42e1e4c780f18
Fuuma <masa81hiro69@gmail.com>
2023-05-14 03:06:53 +0900

    [add]ログイン機能

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const connection = mysql.createConnection({
   database: "zanpy",
 });
 
+// テスト
 app.get("/api", (req, res) => {
   connection.query("SELECT * FROM `animals`", function (err, results, fields) {
     if (err) {
@@ -21,6 +22,7 @@ app.get("/api", (req, res) => {
   console.log("接続終了(正常)");
 });
 
+// 自分の担当エリアの動物を取得
 app.get("/api/myarea", (req, res) => {
   connection.query("SELECT * FROM `animals`", function (err, results, fields) {
     if (err) {
@@ -32,6 +34,27 @@ app.get("/api/myarea", (req, res) => {
   console.log("接続終了(正常)");
 });
 
+// ログイン機能
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // ユーザー名とパスワードのバリデーションなどを行います
+
+  // ユーザーの認証を行います
+  if (isValidUser(username, password)) {
+    // 認証成功時の処理を行います
+
+    // セッションやトークンの生成、ログイン状態の管理などを行います
+
+    res.json({ message: "ログイン成功" });
+  } else {
+    // 認証失敗時の処理を行います
+
+    res.status(401).json({ message: "ログイン失敗" });
+  }
+});
+
+// 接続確認
 app.listen(port, () => {
   console.log(`listening on *:${port}`);
 });
--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -4,6 +4,27 @@ import Button from "@mui/material/Button";
 
 const LoginForm = () => {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://backend:3004/api/login", {
+        username,
+        password,
+      });
+
+      // ログイン成功時の処理を記述します
+      console.log(response.data.message);
+      // ログイン後のリダイレクト先を設定します
+      navigate("/my-area");
+    } catch (error) {
+      // ログイン失敗時の処理を記述します
+      console.error(error.response.data.message);
+    }
+  };
 
   return (
     <div className='bg-white rounded-3xl mx-auto w-4/12 p-8 shadow-2xl'>
@@ -23,6 +44,7 @@ const LoginForm = () => {
             <input
               name='email'
               className='w-full rounded border bg-form px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring'
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
 
@@ -37,6 +59,7 @@ const LoginForm = () => {
               name='password'
               type='password'
               className='w-full rounded border bg-form px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring'
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <Button
@@ -52,7 +75,7 @@ const LoginForm = () => {
               borderRadius: 10,
             }}
             className='w-6/12 '
-            onClick={() => navigate("/my-area")}
+            onClick={handleLogin}
           >
             ログイン
           </Button>
