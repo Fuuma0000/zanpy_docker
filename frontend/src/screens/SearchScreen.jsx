3d923c92e498850a02ad044079c1e71f4b6c9529
Fuuma <masa81hiro69@gmail.com>
2023-05-14 02:55:16 +0900

    [fix]検索の実装途中

--- frontend/src/screens/SearchScreen.jsx
+++ frontend/src/screens/SearchScreen.jsx
@@ -4,17 +4,39 @@ import SearchBox from "../components/SearchBox";
 import { useSetRecoilState } from "recoil";
 import { myAreaOpenState } from "../atoms/MyAreaOpenState";
 import StatesTable from "../components/StatesTable";
+// import axios from "axios";
+// import { useEffect } from "react";
 
-const SearchScreen = ({ rows, setRows }) => {
+const SearchScreen = () => {
+  // const [searchRows, setSearchRows] = React.useState([]);
+
   const setMyAreaOpen = useSetRecoilState(myAreaOpenState);
   //初回レンダリング時にmyAreaOpenStateをfalseにする
   setMyAreaOpen(false);
-  // TODO:検索ページは検索結果を表示するから、ここでデータを保持する
+
+  // function createData(area, name, area, type, states, memos, memoTime) {
+  //   return { area, name, area, type, states, memos, memoTime };
+  // }
+
+  // //初回レンダリング時にAPIからデータを取得する
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get("/api/search");
+  //       const dataWithKeys = response.data.message.map((item) => createData());
+  //       setSearchRows(dataWithKeys);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+
   return (
     <>
       <Header></Header>
       <SearchBox></SearchBox>
-      <StatesTable rows={rows} setRows={setRows}></StatesTable>
+      <StatesTable></StatesTable>
     </>
   );
 };
